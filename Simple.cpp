80753931f74784fccd3f9fa6256666ceb0d3cb43
Chris <cyoung@trantor.(none)>
2013-10-10 16:26:32 -0500

    More logic updates.

--- Simple.cpp
+++ Simple.cpp
@@ -9,6 +9,7 @@ int main()
   Class1 c1(5);
   std::cout << "Hello!" << std::endl << std::endl;
   std::cout << c1.ToString() << std::endl << std::endl;
+  std::cout << "Done!" << std::endl;
 
   return 0;
 }

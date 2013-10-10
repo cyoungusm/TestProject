6a82ddf973e06e9df688ce92ffb1db1991c79c40
Chris <cyoung@trantor.(none)>
2013-10-10 16:27:40 -0500

    More logic.

--- Simple.cpp
+++ Simple.cpp
@@ -7,8 +7,10 @@ int main();
 int main()
 {
   Class1 c1(5);
+  Class2 c2(1);
   std::cout << "Hello!" << std::endl << std::endl;
   std::cout << c1.ToString() << std::endl << std::endl;
+  std::cout << c2.ToString() << std::endl << std::endl;
   std::cout << "Done!" << std::endl;
 
   return 0;
